3122f736f446f28198e9745863c971a25de0d47a
Satoshi Ohmori <sachin21dev@gmail.com>
2014-08-23 21:54:32 +0900

    Refactor for command

--- setup.sh
+++ setup.sh
@@ -13,11 +13,18 @@ if [ $flag = 'm' -o $flag = 'M' ]; then
 elif [ $flag = 'c' -o $flag = 'C' ]; then
   sudo yum -y update
   sudo yum -y upgrade
-  for package in `cat ./package_lists/yum`; do; yum -y install $package; done
+
+  for package in `cat ./package_lists/yum`; do
+    sudo yum -y install $package
+  done
+
   sudo yum -y clean
 elif [ $flag = 'a' -o $flag = 'A' ]; then
   sudo pacman -Sy
-  for package in `cat ./package_lists/pacman`; do; sudo pacman -S $package; done
+
+  for package in `cat ./package_lists/pacman`; do
+    sudo pacman -S $package
+  done
 else
   echo "You can input is only 'm', 'c' and 'a'"
   exit 1

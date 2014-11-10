3c78e459031094687b20ab8fbe135d8d45281d6f
Satoshi Ohmori <sachin21dev@gmail.com>
2014-11-10 09:53:06 +0900

    Remove unuse line

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ if [ $flag = 'm' -o $flag = 'M' ]; then # for Mac OSX
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
   cd ~/dotfiles
   brew bundle
-  cp zsh.dot/zshrc.local.sample zsh.dot/zshrc.local
 elif [ $flag = 'c' -o $flag = 'C' ]; then # for CentOS
   sudo yum -y update
   sudo yum -y upgrade

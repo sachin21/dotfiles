0d33e8e730f3fab91f9471baabcba9146769065f
Satoshi Ohmori <sachin21dev@gmail.com>
2015-01-23 14:37:47 +0900

    Use camel case in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
-# init other packages
+# Initialize other packages
 git submodule init
 git submodule update
 
-# check os
+# Check os
 echo "MacOS user 'm' : Centos user 'c' : ArchLinux user [m/c/a] : "
   read flag
 
-# setup tools
+# Setup tools
 if [ $flag = 'm' -o $flag = 'M' ]; then # For Mac OSX
   cd ~/dotfiles
   ./Homebrew/install.sh
@@ -39,7 +39,7 @@ else
 fi
 echo "Tools was successfully installed."
 
-# install oh-my-zsh
+# Install oh-my-zsh
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 echo "oh-my-zsh was successfully installed."
 
@@ -47,17 +47,17 @@ echo "oh-my-zsh was successfully installed."
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
 echo "NeoBundle was successfully installed."
 
-# install rbenv
+# Install rbenv
 [ ! -d ~/.rbenv ] && git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 [ ! -d ~/.rbenv/plugins/ruby-build ] && git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 ~/.rbenv/plugins/ruby-build/install.sh
 echo "rbenv was successfully installed."
 
-# install pyenv
+# Install pyenv
 [ ! -d ~/.pyenv ] && git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 echo "pyenv was successfully installed."
 
-# install nodenv
+# Install nodenv
 [ ! -d ~/.nodenv ] && git clone https://github.com/OiNutter/nodenv.git ~/.nodenv
 [ ! -d ~/.nodenv/.nodenv/plugins/node-build ] && git clone git://github.com/OiNutter/node-build.git ~/.nodenv/plugins/node-build
 ~/.nodenv/plugins/node-build/install.sh
@@ -66,7 +66,7 @@ echo "nodenv was successfully installed."
 echo "If you want to create projects of sachin21? [y/Y]"
   read flag
 
-# setup repositories
+# Setup repositories
 if [ $flag = 'y' -o $flag = 'Y' ] && type ghq > /dev/null 2>&1; then
   for repository in `cat repositories/github`; do
     ghq get $repository
@@ -75,11 +75,11 @@ else
   echo "[Warning] ghq is not installed" 1>&2
 fi
 
-# create symbolics
+# Create symbolics
 rake clean
 rake all
 
-# reload shell
+# Reload shell
 exec $SHELL
 
 echo "Your shell was reloaded"

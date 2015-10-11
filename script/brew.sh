82ab837cf95f7d7a2d82d15fe5d2eed909749564
Satoshi Ohmori <sachin21dev@gmail.com>
2015-10-12 02:32:00 +0900

    Rename helper file

--- script/brew.sh
+++ script/brew.sh
@@ -5,7 +5,7 @@ set -e
 export DOTFILES_PATH="$HOME/dotfiles"
 
 # Loading method for printing
-. $DOTFILES_PATH/etc/printing_helper
+. $DOTFILES_PATH/etc/print_helper
 
 # Check exist brew command
 if type brew > /dev/null 2>&1; then

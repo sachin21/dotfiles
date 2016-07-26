884f85041de10620866b4fef9223f49ffeabc936
Satoshi Ohmori <sachin21dev@gmail.com>
2016-07-26 23:43:38 +0900

    Huge update for OS X

--- script/bootstrap.zsh
+++ script/bootstrap.zsh
@@ -5,75 +5,82 @@
 # Get ready to develop something
 #
 
-declare DOTFILES_PATH
-declare NEEDED_PACKAGES
-
-DOTFILES_PATH="$HOME/dotfiles"
-NEEDED_PACKAGES="ruby git curl"
+declare DOTFILES_PATH="$HOME/dotfiles"
+declare NEEDED_PACKAGES="ruby git curl"
 
 function install_dotfiles(){
   if [ -d "$DOTFILES_PATH" ]; then
-    cd "$DOTFILES_PATH" && . ./etc/helpers || exit 1
+    . "$DOTFILES_PATH"/etc/helpers
     message "  + Dotfiles is Already exists. Let's go next step"
   else
-    if type git &> /dev/null; then
-      git clone https://github.com/sachin21/dotfiles.git "$DOTFILES_PATH"
-    else
-      echo "Git is required. Please install the git."; exit 1
-    fi
+    if type git > /dev/null 2>&1; then
+      git clone https://github.com/sachin21/dotfiles.git "$DOTFILES_PATH" > /dev/null 2>&1
+      . "$DOTFILES_PATH"/etc/helpers
 
-    cd "$DOTFILES_PATH" && . ./etc/helpers || exit 1
+      succeed "  + Dotfiles was successfully downloaded"
+    else
+      echo "  x [Error] Git is required. Please install the git."; exit 1
+    fi
   fi
+}
+
+function cd_dotfiles() {
+  cd $DOTFILES_PATH
 }
 
 # Initialize other packages
 function initialize_submodules(){
-  message "  + Initializing git packages"
-  git submodule update --init
+  message "  + Initializing git submodules"
+
+  if git submodule update --init > /dev/null 2>&1; then
+    succeed "  + Submodule successfully Initialized"
+  else
+    fail "  + [Error] Submodules unsuccessfully Initialized"; exit 1
+  fi
 }
 
 function install_packages(){
-  # Checking os
-  ask "  + OSX user 'm' : CentOS user 'c' : ArchLinux user 'a' : Ubuntu user 'u' [m/c/a/u]" && read -r flag
-
   # Setup tools
-  if [ "$flag:l" = "m" ]; then # For OS X
-    cd "$DOTFILES_PATH" || exit 1
-    message "  + Installing Homebrew..."
-    ./script/brew.zsh
-  elif [ "$flag:l" = "c" ]; then # For CentOS
-    message "  + Updating already exists packages..."
-    sudo yum -y update || true
-
-    message "  + Upgrading packages..."
-    sudo yum -y upgrade || true
-
-    message "  + Installing needed packages..."
-    sudo yum -y install $NEEDED_PACKAGES || true
-
-    message "  + Cleaning packages..."
-    sudo yum -y clean
-  elif [ "$flag:l" = "a" ]; then # For ArchLinux
-    message "  + Upgrading packages..."
-    sudo pacman -Sy || true
-
-    message "  + Installing needed packages..."
-    sudo pacman -S $NEEDED_PACKAGES
-  elif [ "$flag:l" = 'u' ]; then
-    message " + Updating packages..."
-    sudo apt-get update || true
-
-    message " + Upgrading packages..."
-    sudo apt-get upgrade || true
-
-    message "  + Installing needed packages..."
-    sudo apt-get install -y $NEEDED_PACKAGES || true
+  if [ "$(uname)" == 'Darwin' ]; then
+    if ! ./script/brew.zsh; then
+      fail "  x [Error] Homebrew was unsuccessfully installed", exit 1
+    fi
   else
-    fail "  x [Error] You can input is only 'm', 'c' and 'a'"
-    exit 1
+    # Checking os
+    ask "  + CentOS user 'c' : ArchLinux user 'a' : Ubuntu user 'u' [c/a/u]" && read -r flag
+    if [ "$flag:l" = "c" ]; then # For CentOS
+      message "  + Updating already exists packages..."
+      sudo yum -y update > /dev/null 2>&1 || true
+
+      message "  + Upgrading packages..."
+      sudo yum -y upgrade > /dev/null 2>&1 || true
+
+      message "  + Installing needed packages..."
+      sudo yum -y install $NEEDED_PACKAGES > /dev/null 2>&1 || true
+
+      message "  + Cleaning packages..."
+      sudo yum -y clean > /dev/null 2>&1
+    elif [ "$flag:l" = "a" ]; then # For ArchLinux
+      message "  + Upgrading packages..."
+      sudo pacman -Sy > /dev/null 2>&1 || true
+
+      message "  + Installing needed packages..."
+      sudo pacman -S $NEEDED_PACKAGES
+    elif [ "$flag:l" = 'u' ]; then
+      message " + Updating packages..."
+      sudo apt-get update > /dev/null 2>&1 || true
+
+      message " + Upgrading packages..."
+      sudo apt-get upgrade > /dev/null 2>&1 || true
+
+      message "  + Installing needed packages..."
+      sudo apt-get install -y $NEEDED_PACKAGES > /dev/null 2>&1 || true
+    else
+      fail "  x [Error] You can input is only 'm', 'c' and 'a'"; exit 1
+    fi
   fi
 
-  message "  + Tools was successfully installed"
+  succeed "  + Tools was successfully installed"
 }
 
 function install_linux_brew() {
@@ -89,18 +96,11 @@ function install_omz(){
   if [ -d "$HOME/.oh-my-zsh" ]; then
     message "  * oh-my-zsh is exists."
   else
-    git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
-    message "  + oh-my-zsh was successfully installed"
-  fi
-}
-
-# Install NeoBundle
-function install_neobundle(){
-  if [ -d "$HOME/.vim/bundle"] && [ -d "$HOME/.vim/bundle/neobundle.vim" ]; then
-    message "  + NeoBundle is exist."
-  else
-    mkdir -p "$HOME/.vim/bundle" && git clone git://github.com/Shougo/neobundle.vim "$HOME/.vim/bundle/neobundle.vim"
-    message "  + NeoBundle was successfully installed"
+    if git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" > /dev/null 2>&1; then
+      succeed "  + oh-my-zsh was successfully installed"
+    else
+      fail "  x [Error] oh-my-zsh was unsuccessfully installed", exit 1
+    fi
   fi
 }
 
@@ -109,17 +109,21 @@ function install_rbenv(){
   if [ -d "$HOME/.rbenv" ]; then
     message "  + rbenv is exist."
   else
-    git clone https://github.com/rbenv/rbenv.git "$HOME/.rbenv"
+    if git clone https://github.com/rbenv/rbenv.git "$HOME/.rbenv" > /dev/null 2>&1; then
+      succeed "  + rbenv was successfully installed"
+    else
+      fail "  x [Error] rbenv was unsuccessfully installed", exit 1
+    fi
 
     if [ -d "$HOME/.rbenv/plugins/ruby-build" ]; then
       message "  + ruby-build is exist."
     else
-      git clone https://github.com/rbenv/ruby-build.git "$HOME/.rbenv/plugins/ruby-build"
-      "$HOME/.rbenv/plugins/ruby-build/install.sh"
-      message "  + ruby-build was successfully installed"
+      if git clone https://github.com/rbenv/ruby-build.git "$HOME/.rbenv/plugins/ruby-build" > /dev/null 2>&1 && "$HOME/.rbenv/plugins/ruby-build/install.sh" > /dev/null 2>&1; then
+        succeed "  + ruby-build was successfully installed"
+      else
+        fail "  x [Error] ruby-build was unsuccessfully installed", exit 1
+      fi
     fi
-
-    message "  + rbenv was successfully installed"
   fi
 }
 
@@ -128,8 +132,11 @@ function install_pyenv(){
   if [ -d "$HOME/.pyenv" ]; then
     message "  + pyenv is exists"
   else
-    git clone git://github.com/yyuu/pyenv.git "$HOME/.pyenv"
-    message "  + pyenv was successfully installed"
+    if git clone git://github.com/yyuu/pyenv.git "$HOME/.pyenv" > /dev/null 2>&1; then
+      succeed "  + pyenv was successfully installed"
+    else
+      fail "  x [Error] pyenv was unsuccessfully installed", exit 1
+    fi
   fi
 }
 
@@ -138,79 +145,94 @@ function install_nodenv(){
   if [ -d "$HOME/.nodenv" ]; then
     message "  + nodenv is exists"
   else
-    git clone https://github.com/OiNutter/nodenv.git "$HOME/.nodenv"
+    if git clone https://github.com/OiNutter/nodenv.git "$HOME/.nodenv" > /dev/null 2>&1; then
+      succeed "  + nodenv was successfully installed"
+    else
+      fail "  x [Error] nodenv was unsuccessfully installed", exit 1
+    fi
 
     if [ -d "$HOME/.nodenv/.nodenv/plugins/node-build" ]; then
       message "  + node-build is exists"
     else
-      git clone git://github.com/OiNutter/node-build.git "$HOME/.nodenv/plugins/node-build"
-      "$HOME/.nodenv/plugins/node-build/install.sh"
-      message "  + node-build was successfully installed"
+      if git clone git://github.com/OiNutter/node-build.git "$HOME/.nodenv/plugins/node-build" > /dev/null 2>&1 && "$HOME/.nodenv/plugins/node-build/install.sh" > /dev/null 2>&1; then
+        succeed "  + node-build was successfully installed"
+      else
+        fail "  x [Error] nodenv was unsuccessfully installed", exit 1
+      fi
     fi
-
-    message "  + nodenv was successfully installed"
   fi
 }
 
 # Setup repositories
 function install_ghq() {
-  if command_not_exists curl; then
-    exit
+  if command_not_exists curl > /dev/null 2>&1; then
+    fail "  x [Error] cURL is not installed"; return 1
   fi
 
   ask "  + If you want to create projects of sachin21? [y/Y]" && read -r flag
 
   if [ "$flag:l" = "y" ]; then
-    if command_not_exists ghq; then
+    if command_exists ghq > /dev/null 2>&1; then
       message "  + Installing ghq..."
-      brew tap motemen/ghq
-      brew install ghq
+      if brew tap motemen/ghq > /dev/null 2>&1 && brew install ghq > /dev/null 2>&1; then
+        succeed "  + ghq was successfully installed"
+      fi
     fi
 
     message "  + tapping repositories..."
     curl -fsSL https://api.github.com/users/sachin21/repos \
       | ruby -rjson -e 'JSON.parse(STDIN.read).each{ |r| puts r["name"] }' \
         | while read -r repository; do
-      ghq get "https://github.com/sachin21/$repository.git"
+      message "  + Cloning $repository..."
+      ghq get "https://github.com/sachin21/$repository.git" || true
     done
   fi
 }
 
 function install_zplug() {
-  git clone https://github.com/b4b4r07/zplug "$HOME/.zplug"
+  git clone https://github.com/b4b4r07/zplug "$HOME/.zplug" > /dev/null 2>&1
 }
 
 function create_symbolic_links(){
-  if command_exists rake; then
-    message "  + Executing rake tasks..."
-    rake clean
-    rake deploy
-  else
-    warn "  x [Warning] rake is not installed"
+  if command_not_existsrake > /dev/null 2>&1; then
+    warn "  x [Warning] rake is not installed"; return 1
+  fi
+
+  message "  + Executing rake tasks..."
+  rake clean > /dev/null 2>&1
+  if ! rake deploy > /dev/null 2>&1; then
+    fail '  x Dotfiles was unsuccessfully deployed'; return 1
   fi
 }
 
 function create_needed_dirs() {
-  mkdir "$HOME/.go"
+  if ! [ -d "$HOME/.go" ]; then
+    mkdir "$HOME/.go"
+  fi
 }
 
 function create_needed_files() {
   : > "$DOTFILES_PATH/zsh.dot/zshrc.local"
 }
 
-function print_after_steps(){
-  succeed
-  succeed "  Your shell was successfully reloaded."
-  succeed "  It's all done."
-  succeed
-  succeed "  ** After setup: You need to change shell **"
-  succeed "  Add $(/usr/bin/which zsh) path to /etc/shells, and"
-  succeed "  Execute chsh -s $(command which zsh)"
-  succeed
+function change_default_shell() {
+  message '  + Changing default shell...'
+
+  if [ -d /usr/local/bin/zsh ]; then
+    sudo echo "/usr/local/bin/zsh" >> /etc/shells
+    chsh -s /usr/local/bin
+  else
+    chsh -s /bin/zsh
+  fi
+}
+
+function change_shell_to_zsh() {
+  /bin/zsh
 }
 
 function main(){
   install_dotfiles
+  cd_dotfiles
   initialize_submodules
 
   while ! install_packages; do
@@ -222,7 +244,6 @@ function main(){
   fi
 
   install_omz
-  install_neobundle
   install_rbenv
   install_pyenv
   install_nodenv
@@ -231,8 +252,8 @@ function main(){
   create_symbolic_links
   create_needed_dirs
   create_needed_files
-
-  print_after_steps
+  change_default_shell
+  change_shell_to_zsh
 }
 
 main

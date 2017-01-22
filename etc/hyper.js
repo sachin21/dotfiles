bf4baea281ae893d89fa85ce1c5c05c7bcfdc782
Satoshi Ohmori <sachin21dev@gmail.com>
2017-01-22 20:01:08 +0900

    Modify settings related to the font

--- etc/hyper.js
+++ etc/hyper.js
@@ -1,10 +1,10 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 15,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: 'ricty',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.75)',
